c13fd9eb701eaeafc7da38ed03e7465dbdb03357
Daniel Furman <djfurman@gmail.com>
2018-07-02 20:49:04 -0400

    Add Nuxt.js project for CIDR calculator. Initial commit.

---
+++ components/Calculator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" name="first_octet" id="first_octet" v-model="octets.first">.
+    <input type="text" name="second_octet" id="second_octet" v-model="octets.second">.
+    <input type="text" name="third_octet" id="third_octet" v-model="octets.third">.
+    <input type="text" name="fourth_octet" id="fourth_octet" v-model="octets.fourth">/
+    <input type="text" name="subnet" id="subnet" v-model="subnet">
+    <input type="button" value="Submit" @click="sendCidr">
+
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cidr() {
+      return `${this.octets.first}.${this.octets.second}.${this.octets.third}.${this.octets.fourth}/${this.subnet}`;
+    }
+  },
+
+  data() {
+    return {
+      octets: {
+        first: '10',
+        second: '0',
+        third: '0',
+        fourth: '0',
+      },
+      subnet: '16'
+    }
+  },
+
+  methods: {
+    sendCidr() {
+      console.log(this.cidr);
+    }
+  }
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        cidr-calculator
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js experiment project to implement a front end for a simple serverless CIDR calculator
+      </h2>
+      <calculator/>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import Calculator from '~/components/Calculator.vue'
+
+export default {
+  components: {
+    AppLogo,
+    Calculator
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
